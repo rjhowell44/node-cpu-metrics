18a175de50ebe310a4c7045dba6e016c2fcc63e7
Robert Howell <rjhowell44@gmail.com>
2017-05-05 05:11:05 +0000

    reduced heap usage for graph start

--- Test/consume_heap.js
+++ Test/consume_heap.js
@@ -6,16 +6,17 @@ function consume_heap()
     var garbage = [];
     
     
-    for (var i=0; i<100000; i++)
+    for (var i=0; i<1000; i++)
     {
-        garbage.push("this is a long string to consume heap memory.... lets make the string even longer... why stop here... even longer still");
+        garbage.push("this is a long string to consume heap memory");
 
     }
 }
 
+
 describe('DumpHeapMetrics', function(){
   it('should return true', function(){
-    for(var i=0;i<100;i++){  
+    for(var i=0;i<10;i++){  
         consume_heap();  
     }
     assert(heap_metrics.DumpHeapMetrics());
