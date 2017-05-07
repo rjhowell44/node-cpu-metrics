b2acb907f3b01d54e60e413a30188d5a4b96fcab
Robert Howell <rjhowell44@gmail.com>
2017-05-07 05:01:19 +0000

    rebuild for newer node v

---
+++ build/Release/.deps/Release/obj.target/heap_metrics/src/heap_metrics.o.d
@@ -0,0 +1,24 @@
+cmd_Release/obj.target/heap_metrics/src/heap_metrics.o := g++ '-DNODE_GYP_MODULE_NAME=heap_metrics' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/ubuntu/.node-gyp/6.10.3/include/node -I/home/ubuntu/.node-gyp/6.10.3/src -I/home/ubuntu/.node-gyp/6.10.3/deps/uv/include -I/home/ubuntu/.node-gyp/6.10.3/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/heap_metrics/src/heap_metrics.o.d.raw   -c -o Release/obj.target/heap_metrics/src/heap_metrics.o ../src/heap_metrics.cpp
+Release/obj.target/heap_metrics/src/heap_metrics.o: \
+ ../src/heap_metrics.cpp ../src/heap_metrics.h \
+ /home/ubuntu/.node-gyp/6.10.3/include/node/v8.h \
+ /home/ubuntu/.node-gyp/6.10.3/include/node/v8-version.h \
+ /home/ubuntu/.node-gyp/6.10.3/include/node/v8config.h \
+ /home/ubuntu/.node-gyp/6.10.3/include/node/v8-profiler.h \
+ /home/ubuntu/.node-gyp/6.10.3/include/node/v8.h \
+ /home/ubuntu/.node-gyp/6.10.3/include/node/node.h \
+ /home/ubuntu/.node-gyp/6.10.3/include/node/node_version.h \
+ /home/ubuntu/.node-gyp/6.10.3/include/node/node_object_wrap.h \
+ ../src/heap_metrics_type.h ../src/heap_metrics_table.h
+../src/heap_metrics.cpp:
+../src/heap_metrics.h:
+/home/ubuntu/.node-gyp/6.10.3/include/node/v8.h:
+/home/ubuntu/.node-gyp/6.10.3/include/node/v8-version.h:
+/home/ubuntu/.node-gyp/6.10.3/include/node/v8config.h:
+/home/ubuntu/.node-gyp/6.10.3/include/node/v8-profiler.h:
+/home/ubuntu/.node-gyp/6.10.3/include/node/v8.h:
+/home/ubuntu/.node-gyp/6.10.3/include/node/node.h:
+/home/ubuntu/.node-gyp/6.10.3/include/node/node_version.h:
+/home/ubuntu/.node-gyp/6.10.3/include/node/node_object_wrap.h:
+../src/heap_metrics_type.h:
+../src/heap_metrics_table.h:
