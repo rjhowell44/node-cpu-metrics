94a05ae4520010e71403796b9e694740b5df61da
Robert Howell <rjhowell44@gmail.com>
2017-05-10 04:14:51 +0000

    export functions in index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 var heap_metrics = require('./build/Release/heap_metrics.node');
 
-console.log(heap_metrics.GetHeapMetrics());
+module.exports = function GetHeapMetrics(){
+  heap_metrics.GetHeapMetrics();
+};  
+
+module.exports = function DumHeapMetrics(){
+  heap_metrics.GetHeapMetrics();
+};  
