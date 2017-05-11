88c6ffef9e4c57f09ca2b224c5bebb74be6bae2d
Robert Howell <rjhowell44@gmail.com>
2017-05-11 16:42:52 +0000

    exporting wrapper in index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 var heapMetrics = require('./build/Release/heap_metrics.node');
+
+module.exports = function getHeapMetrics()
+{
+    return heapMetrics.GetHeapMetrics();
+};
+
+module.exports = function dumpHeapMetrics()
+{
+    return heapMetrics.DumpHeapMetrics();
+};
