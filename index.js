5544cb7b42c2292912232a628a9d9c33fa0914c0
Robert Howell <rjhowell44@gmail.com>
2017-05-11 01:25:55 +0000

    Added xunit reporter for mocha. Include test results in push to Testspace

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-var heap_metrics = require('./build/Release/heap_metrics.node');
+var heapMetrics = require('./build/Release/heap_metrics.node');
 
-module.exports = function GetHeapMetrics(){
-  heap_metrics.GetHeapMetrics();
-};  
-
-module.exports = function DumHeapMetrics(){
-  heap_metrics.GetHeapMetrics();
-};  
+console.log(heapMetrics.GetHeapMetrics());
